5d9da0322033c9b09912962bda837c8191aac1b6
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-02-06 17:00:39 +0800

    fix: qrcode errors and refactor secretkey generator

--- app/api/admin/actions.ts
+++ app/api/admin/actions.ts
@@ -4,12 +4,41 @@ import QRCode from "qrcode";
 export const updateVisitorApprovalStatus = async (
   id: number,
   status: string,
-  email: string
+  email: string,
+  user_id: string
 ) => {
+  const generateRandomKey = (length: number): string => {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    const phraseToHide = "H!H4ck3r";
+
+    if (length < phraseToHide.length) {
+      throw new Error(
+        "Length must be greater than or equal to the hidden phrase length."
+      );
+    }
+
+    let result = "";
+    for (let i = 0; i < length - phraseToHide.length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    const randomIndex = Math.floor(
+      Math.random() * (length - phraseToHide.length + 1)
+    );
+    result =
+      result.slice(0, randomIndex) + phraseToHide + result.slice(randomIndex);
+
+    return result;
+  };
+
+  const secretKey = generateRandomKey(20);
+
   const supabase = createClient();
   const { data, error } = await supabase
-    .from("visitor_approvals")
-    .update({ status })
+    .from("guest_parking_request")
+    .update({ status, secret_key: secretKey })
     .eq("id", id);
 
   if (error) {
@@ -17,35 +46,24 @@ export const updateVisitorApprovalStatus = async (
     return null;
   }
 
-  const generateRandomKey = (length: number) => {
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result.replace(/Hi Hacker/g, "");
-  };
-
   const qrData = {
     id: id,
-    email: email,
+    user_id: user_id,
     status: status,
-    secret_key: generateRandomKey(16),
+    secret_key: secretKey,
   };
 
   const qrCodeDataUrl = await QRCode.toDataURL(JSON.stringify(qrData));
 
-  interface EmailPayload {
-    to: string;
-    subject: string;
-    html: string;
-    attachments?: {
-      filename: string;
-      content: string;
-      encoding: string;
-    }[];
+  const { error: qrError } = await supabase.from("guest_qr_codes").insert({
+    user_id: user_id,
+    qr_code_url: qrCodeDataUrl,
+    secret_key: qrData.secret_key,
+  });
+
+  if (qrError) {
+    console.error("Error inserting QR code data:", qrError);
+    return null;
   }
 
   const emailPayload: EmailPayload = {
